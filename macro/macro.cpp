58c2ce5d1ec0a97a7ef83451d6f1911869d2cab6
eshwar <eshwarmsms@gmail.com>
2017-04-14 11:29:50 +0530

    macros - better handling of files

--- macro/macro.cpp
+++ macro/macro.cpp
@@ -5,71 +5,69 @@
 
 using namespace std;
 
- string label, mnumonic, operand, data;
- string macroName;
+string label, mnumonic, operand, data;
+string macroName;
 
- ifstream input("input.txt");
- ofstream output("ouptut.txt");
- fstream macro("macro.txt");
+ifstream input("input.txt");
+ofstream output("output.txt");
+fstream macro("macro.txt");
 
- void getData(){
-     label ="";
-     mnumonic="";
-     operand = "";
-     getline(input, data);
-     stringstream s(data);
-     getline(s, label, '\t');
-     getline(s, mnumonic, '\t');
-     getline(s, operand, '\t');
+void getData(){
+	label ="";
+	mnumonic="";
+	operand = "";
+	getline(input, data);
+	stringstream s(data);
+	getline(s, label, '\t');
+	getline(s, mnumonic, '\t');
+	getline(s, operand, '\t');
  }
 
  int main(){
+	getData();
+	while(!input.eof()){
+	if(!mnumonic.compare("MACRO")){
+		macroName = label;
+		getData();
+			while(mnumonic.compare("MEND")){
+				macro<<label<<'\t'<<mnumonic<<'\t'<<operand<<endl;
+				getData();
+			}
+		}
+		getData();
+	}
 
-    getData();
-    while(!input.eof()){
-        if(!mnumonic.compare("MACRO")){
-            macroName = label;
-            getData();
-            while(mnumonic.compare("MEND")){
-                macro<<label<<'\t'<<mnumonic<<'\t'<<operand<<endl;
-                getData();
-            }
-        }     
-        getData();
-     }
+	input.clear();
+	input.seekg(0, ios::beg);
+	macro.clear();
+	macro.seekg(0, ios::beg);
 
-     input.close();
-     macro.close();
-     input.open("input.txt");
-     macro.open("macro.txt");
+	getData();
+	while(!input.eof()){
+		if(!mnumonic.compare(macroName)){
+			if(!label.empty()){
+				output<<label;
+			}
+			while(!macro.eof()){
+				getline(macro, data);
+				output<<data<<endl;
+			}
+				output.seekp(-1, ios::cur);
+				macro.clear();
+				macro.seekg(0, ios::beg);
+		}
+		else if(!mnumonic.compare("MACRO")){
+			while(mnumonic.compare("MEND")){
+				getData();
+			}
+		}
+		else{
+			output<<label<<'\t'<<mnumonic<<'\t'<<operand<<endl;
+		}
+		getData();
+	}
 
-     getData();
-     while(!input.eof()){
-        if(!mnumonic.compare(macroName)){
-            if(!label.empty()){
-                output<<label;
-            }
-            while(!macro.eof()){
-                getline(macro, data);
-                output<<data<<endl;
-            }
-            cout<<output.tellp()<<endl;
-            output.seekp(-1, ios::cur);
-            macro.close();
-            macro.open("macro.txt");
-        }  
-        else if(!mnumonic.compare("MACRO")){
-            while(mnumonic.compare("MEND")){
-                getData();
-            }
-        }  
-        else{
-            output<<label<<'\t'<<mnumonic<<'\t'<<operand<<endl;
-        } 
-        getData();
-     }
-
-     input.close();
-     output.close();
-     macro.close();
+	input.close();
+	output.close();
+	macro.close();
  }
